54cabfec8a6933cafb2bf3a37b03ffbfb97082e6
nzambello <nzambello@protonmail.com>
2022-03-10 21:19:21 +0100

    fix: model load

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
+import { OrbitControls } from '@react-three/drei'
 import Model from './Model'
 import Loader from './Loader'
 
 export default function App() {
   return (
     <div className="App">
-      <Canvas>
+      <Canvas
+        camera={{ position: [2, 0, 12.25], fov: 15 }}
+        style={{
+          backgroundColor: '#fff',
+          width: '100vw',
+          height: '100vh'
+        }}
+      >
+        <ambientLight intensity={1.25} />
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={0.4} />
         <Suspense fallback={<Loader />}>
-          <Model position={[1, -4, 2]} scale={7} />
+          <Model position={[0.025, -0.9, 0]} />
         </Suspense>
+        <OrbitControls />
       </Canvas>
     </div>
   )
